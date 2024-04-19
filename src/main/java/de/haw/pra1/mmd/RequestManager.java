9f112c72f9cb906198affd83dc2228f341895547
Matthias Raimann <matthias.raimann82@gmail.com>
2024-04-19 13:36:22 +0200

    100 percent test coverage

--- src/main/java/de/haw/pra1/mmd/RequestManager.java
+++ src/main/java/de/haw/pra1/mmd/RequestManager.java
@@ -1,10 +1,11 @@
 package de.haw.pra1.mmd;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import java.util.logging.Logger;
 
-public class RequestManager {
+public class RequestManager implements JavaDelegate {
     private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
 
     public void execute(DelegateExecution execution) throws Exception {
--- src/test/java/de/haw/pra1/mmd/ProcessUnitTest.java
+++ src/test/java/de/haw/pra1/mmd/ProcessUnitTest.java
@@ -1,10 +1,19 @@
 package de.haw.pra1.mmd;
 
+import org.assertj.core.api.Condition;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.impl.util.ClockUtil;
+import org.camunda.bpm.engine.runtime.Job;
+import org.camunda.bpm.engine.runtime.MessageCorrelationBuilder;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.community.process_test_coverage.junit5.platform7.ProcessEngineCoverageExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
@@ -14,15 +23,200 @@ import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 @ExtendWith(ProcessEngineCoverageExtension.class)
 public class ProcessUnitTest {
 
-//  @Test
-//  @Deployment(resources = "process.bpmn")
-//  public void testHappyPath() {
-//    // Drive the process by API and assert correct behavior by camunda-bpm-assert
-//
-//    ProcessInstance processInstance = processEngine().getRuntimeService()
-//        .startProcessInstanceByKey(ProcessConstants.PROCESS_DEFINITION_KEY);
-//
-//    assertThat(processInstance).isEnded();
-//  }
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testHappyPath() {
+    TaskService taskService = processEngine().getTaskService();
 
+
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+        .createMessageCorrelation("Antragseingang")
+            .setVariable("DatumBescheid", new Date())
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlateStartMessage();
+
+    processEngine().getRuntimeService().createMessageCorrelation("AkteneinsichtGewährt")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+
+    Task t = taskService.createTaskQuery().active().taskName("Begruendung schreiben").singleResult();
+    taskService.complete(t.getId());
+
+    processEngine().getRuntimeService().createMessageCorrelation("Behoerdenantwort")
+            .processInstanceBusinessKey("myBusinessKey")
+            .setVariable("WiderspruchAbgeholfen", true)
+            .correlate();
+
+    assertThat(processInstance).isEnded();
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testDateNotValid() {
+    Calendar calendar= Calendar.getInstance();
+    calendar.add(Calendar.MONTH, 2);
+
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .createMessageCorrelation("Antragseingang")
+            .setVariable("DatumBescheid", calendar.getTime())
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlateStartMessage();
+
+    assertThat(processInstance).isEnded();
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testFirstRejectRequest() throws Exception {
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .createMessageCorrelation("Antragseingang")
+            .setVariable("DatumBescheid", new Date())
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlateStartMessage();
+
+    processEngine().getRuntimeService().createMessageCorrelation("Kunde Abbruchwunsch")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+    assertThat(processInstance).isEnded();
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testSecondRejectRequest() {
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .createMessageCorrelation("Antragseingang")
+            .setVariable("DatumBescheid", new Date())
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlateStartMessage();
+
+    processEngine().getRuntimeService().createMessageCorrelation("AkteneinsichtGewährt")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+
+    TaskService taskService = processEngine().getTaskService();
+
+    processEngine().getRuntimeService().createMessageCorrelation("Kunde Abbruchwunsch")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+    assertThat(processInstance).isEnded();
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testThirdRejectRequest() {
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .createMessageCorrelation("Antragseingang")
+            .setVariable("DatumBescheid", new Date())
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlateStartMessage();
+
+    processEngine().getRuntimeService().createMessageCorrelation("AkteneinsichtGewährt")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+
+    TaskService taskService = processEngine().getTaskService();
+    Task t = taskService.createTaskQuery().active().taskName("Begruendung schreiben").singleResult();
+    taskService.complete(t.getId());
+
+    processEngine().getRuntimeService().createMessageCorrelation("Kunde Abbruchwunsch")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+    assertThat(taskService.createTaskQuery().active().list().get(0))
+            .is(new Condition<>(task -> task.getName().equals("Verfahren beenden und Rechnung schreiben"), "Richtige Activity!"));
+
+    Task task = taskService.createTaskQuery().active().taskName("Verfahren beenden und Rechnung schreiben").singleResult();
+    taskService.complete(task.getId());
+
+    assertThat(processInstance).isEnded();
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testFirstTimerExceeded() {
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .createMessageCorrelation("Antragseingang")
+            .setVariable("DatumBescheid", new Date())
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlateStartMessage();
+
+    Job timer = managementService().createJobQuery().active().timers().singleResult();
+    managementService().executeJob(timer.getId());
+
+    TaskService taskService = processEngine().getTaskService();
+    Task t = taskService.createTaskQuery().active().taskName("Klage auf Akteneinsicht einlegen").singleResult();
+
+    taskService.complete(t.getId());
+
+    assertThat(processInstance).isEnded();
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testSecondTimerExceeded() {
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .createMessageCorrelation("Antragseingang")
+            .setVariable("DatumBescheid", new Date())
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlateStartMessage();
+
+    processEngine().getRuntimeService().createMessageCorrelation("AkteneinsichtGewährt")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+    TaskService taskService = processEngine().getTaskService();
+    Task t = taskService.createTaskQuery().active().taskName("Begruendung schreiben").singleResult();
+    taskService.complete(t.getId());
+
+    Job timer = managementService().createJobQuery().active().timers().singleResult();
+    managementService().executeJob(timer.getId());
+
+    Task t1 = taskService.createTaskQuery().active().taskName("Ungültigkeitsklage einlegen").singleResult();
+    taskService.complete(t1.getId());
+
+    assertThat(processInstance).isEnded();
+  }
+
+  @Test
+  @Deployment(resources = "process.bpmn")
+  public void testObjectionRefused() {
+    TaskService taskService = processEngine().getTaskService();
+
+
+    ProcessInstance processInstance = processEngine().getRuntimeService()
+            .createMessageCorrelation("Antragseingang")
+            .setVariable("DatumBescheid", new Date())
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlateStartMessage();
+
+    processEngine().getRuntimeService().createMessageCorrelation("AkteneinsichtGewährt")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+
+    Task t = taskService.createTaskQuery().active().taskName("Begruendung schreiben").singleResult();
+    taskService.complete(t.getId());
+
+    processEngine().getRuntimeService().createMessageCorrelation("Behoerdenantwort")
+            .processInstanceBusinessKey("myBusinessKey")
+            .setVariable("WiderspruchAbgeholfen", false)
+            .correlate();
+
+    processEngine().getRuntimeService().createMessageCorrelation("AkteneinsichtGewährt")
+            .processInstanceBusinessKey("myBusinessKey")
+            .correlate();
+
+    assertThat(taskService.createTaskQuery().active().list().get(0))
+            .is(new Condition<>(
+                    task -> task.getName().equals("Begruendung schreiben"),
+                    "Es wird wieder auf die 'Begruendung schreiben' gewartet")
+            );
+
+    assertThat(processInstance).isNotEnded();
+  }
 }
